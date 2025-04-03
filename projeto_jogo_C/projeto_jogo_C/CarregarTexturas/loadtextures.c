c6bbdf8bbb8bfb7dd5405ae49ad88ae17486cc4d
lmsm2 <lmsm2@cin.ufpe.br>
2025-04-03 09:27:28 -0300

    Update loadtextures.c

--- projeto_jogo_C/projeto_jogo_C/CarregarTexturas/loadtextures.c
+++ projeto_jogo_C/projeto_jogo_C/CarregarTexturas/loadtextures.c
@@ -5,6 +5,11 @@ void Carregar(){
 
     RedBall = LoadTexture("assets/sprite_RedBall.png");
     BlueBall = LoadTexture("assets/sprite_BlueBall.png");
+    robocin_nivel_1= LoadTexture("assets/robocin_nivel_1.png");
+    robocin_atordoado = LoadTexture ("assets/robocin_atordoado.png");
+    robocin_nivel_2= LoadTexture("assets/robocin_nivel_2.png");
+    robocin_morto= LoadTexture("assets/robocin_morto.png");
+
 
 }
 
@@ -12,5 +17,9 @@ void Descarregar(){
 
     UnloadTexture(RedBall);
     UnloadTexture(BlueBall);
+    UnloadTexture(robocin_nivel_1);
+    UnloadTexture(robocin_atordoado);
+    UnloadTexture(robocin_nivel_2);
+    UnloadTexture(robocin_morto);
 
 }
